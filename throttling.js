51696b382e45184e4b348c29dd4e4b30ac8b6409
WebDevJack <44706909+WebDevJack@users.noreply.github.com>
2020-01-02 12:04:04 +0000

    Update throttling.js

--- throttling.js
+++ throttling.js
@@ -2,8 +2,6 @@
 
   ES6 Throttling the execution of scripts attached to "scroll" DOM events. Drastically reduces load on JS engine, as scroll events 
   can trigger many times on one scroll, E.G on mobile. Throttling limits the amount of times the event can trigger per a set time.
-  
-  The below combines the throttling concept, with an API that allow elements to be slide on when they are in the window "view" (scroll).
 
 */
 
